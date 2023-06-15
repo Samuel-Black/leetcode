f7e35813072a43d552bec2a36721c7fc6ab86e0a
samuel-black <sam.black604@gmail.com>
2023-06-15 12:30:43 +1000

    push

---
+++ kth-largest-element-in-an-array.js
@@ -0,0 +1,29 @@
+const {
+    MinPriorityQueue,
+} = require('@datastructures-js/priority-queue');
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const minHeap = new MinPriorityQueue();
+    
+    const numsLength = nums.length;
+    
+    for (let i = 0; i < numsLength; i++) {
+        const currentMin = minHeap.front();
+        const currentVal = nums[i];
+        if (i < k) {
+            minHeap.enqueue(nums[i]);
+        } else if (currentVal > currentMin) {
+            minHeap.dequeue();
+            minHeap.enqueue(currentVal);
+        }
+    }
+    
+    return minHeap.front();
+};
+
+console.log(findKthLargest([3,2,1,5,6,4], 2));
