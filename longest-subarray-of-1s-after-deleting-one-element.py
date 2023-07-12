b113fa8065bc44d603f250723da96fb7514f30aa
Sam Black <sam.black604@gmail.com>
2023-07-12 12:07:02 +1000

    longest subarray oif 1s after deleting one element

--- longest-subarray-of-1s-after-deleting-one-element.py
+++ longest-subarray-of-1s-after-deleting-one-element.py
@@ -3,32 +3,48 @@ from typing import List
 class Solution:
   def longestSubarray(self, nums: List[int]) -> int:
     
-    left = right = res = 0
-    mid = -1
-    # hasZero = False
+    # left = right = res = 0
+    # mid = -1
+    # # hasZero = False
+    # n = len(nums)
+    
+    # # leftCount = rightCount = 0
+    
+    # while right < n:
+    #   if mid == -1 and nums[right] == 0:
+    #     # deleted = True
+    #     mid = right
+    #   elif mid != -1 and nums[right] == 0:
+    #     # hasZero = True
+    #     left = mid+1
+    #     mid = right
+    #     # deleted = False
+    #     # left
+    #   res = ((right) - (left)) if res < ((right) - (left)) else res
+    #   right+=1
+    
+    # # if mid == -1:
+    # #   return max(0, res - 1)
+    # # else:
+    # return res
+    
+    left = deleted = res = 0
+    zeroIndex = -1
     n = len(nums)
-    
-    # leftCount = rightCount = 0
-    
-    while right < n:
-      if mid == -1 and nums[right] == 0:
-        # deleted = True
-        mid = right
-      elif mid != -1 and nums[right] == 0:
-        # hasZero = True
-        left = mid+1
-        mid = right
-        # deleted = False
-        # left
-      res = ((right) - (left)) if res < ((right) - (left)) else res
-      right+=1
-    
-    # if mid == -1:
-    #   return max(0, res - 1)
-    # else:
+    for right in range(0, n):
+      deleted += 1 - nums[right]
+      if deleted >= 2:
+        left = zeroIndex
+        zeroIndex = right
+        deleted-=1
+      res = max(res, right - left)
+      # if deleted >= 2:
+      #   deleted -= 1 - nums[left]
+      #   left+=1
+        
     return res
     
 solution = Solution()
-# print(solution.longestSubarray([1,1,0,1]))
-# print(solution.longestSubarray([0,1,1,1,0,1,1,0,1]))
+print(solution.longestSubarray([1,1,0,1]))
+print(solution.longestSubarray([0,1,1,1,0,1,1,0,1]))
 print(solution.longestSubarray([1,1,1]))
